4946654e845fd36c04eb72a0c9034a7bc7d8a3d7
Juan Gómez <gb.jc@outlook.com>
2025-09-22 16:43:05 +0000

    Basic connection to Redis & set and get key-value.

--- SportStatsTracker/Program.cs
+++ SportStatsTracker/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StackExchange.Redis;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+internal class Program
+{
+	private static async Task Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!");
+
+		var config = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.Build();
+
+		string redisConnectionString = config["CacheConnection"] ?? throw new InvalidOperationException("Connection string 'CacheConnection' not found.");
+		using (var redisCache = ConnectionMultiplexer.Connect(redisConnectionString))
+		{
+			IDatabase db = redisCache.GetDatabase();
+
+			bool setValue = await db.StringSetAsync("test:key", "some value");
+			Console.WriteLine($"Set value: {setValue}");
+
+			string? getValue = await db.StringGetAsync("test:key");
+			Console.WriteLine($"Get value: {getValue}");
+		}
+	}
+}
